48fa957a7c0f182622a91dfdc08ccb7e9b3ec9fd
Peter Jolley <peterjolley456@gmail.com>
2022-07-23 13:13:45 +0930

    webpack add to and coded

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,56 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      // editor: './src/js/editor.js',
+      // header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin ({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      newInjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE',
+        Short_name: 'JATE',
+        description: 'A beautiful text editor',
+        start_url: '/',
+        publicPath: '/',
+        icons: [{
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96,128,192,256,384,512],
+          destination: path.join('assets', 'icons')
+        },
+      ],
+      }),
     ],
 
     module: {
-      rules: [
-        
+      rules: [{
+        test: /\.css$/i,
+        use: ['style-loader','css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+        },
+      },
       ],
     },
   };
